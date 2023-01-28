853a1be9c8f25e3a764eb40c1ee648eecdabe9c4
Azul Briones <203720@ids.upchiapas.edu.mx>
2023-01-28 15:25:08 -0600

    One document upload added

--- proceso/urls.py
+++ proceso/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include
 
 from proceso.views import ProcesoView,ProcesoDetail
 urlpatterns = [
-    path('list', ProcesoView.as_view()),
+    path('list/', ProcesoView.as_view()),
     path('list/<int:pk>', ProcesoDetail.as_view()),
 ]
