d25de6790000147bc1444f09d82495a6969edb49
Alan Gomez <soyalangg@gmail.com>
2023-02-25 21:13:56 -0600

    removed old url

--- qualitor_backend/urls.py
+++ qualitor_backend/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('api/v1/user/', include('user.urls')),
     path('api/v1/role/', include('role.urls')),
     path('api/v1/proceso/', include('proceso.urls')),
-    path('api/v1/documento/', include('documento.urls')),
     path('api/v1/manual/', include('manual.urls')),
     path('api/v1/proyectos/', include('proyectos.urls')),
     path('api/v1/fases/', include('phases.urls')),
