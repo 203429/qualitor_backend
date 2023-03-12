b520d20bc5ed411eb9894404a078114084b32e82
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-03-11 20:02:21 -0600

    Routes added to User for delete and get by id

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path,re_path
-from .views import UserList
+from .views import UserList, UserListDetail, UserDelete
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('register/', include('dj_rest_auth.registration.urls')),
     re_path('list/', UserList.as_view()),
+    re_path('list/(?P<id_users>\d+)/', UserListDetail.as_view()),
+    re_path('delete/(?P<id_users>\d+)/', UserDelete.as_view()),
 ]
 
 # urlpatterns = [
