11d50a7298b5bfe1ceff0d93b458b83a3eeaef50
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-03-11 19:59:44 -0600

    Method Delete and Get by ID on User

--- user/views.py
+++ user/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 import json
+# from django.contrib.auth.models import User
+from .serializers import CustomUserDetailsSerializer
 
 #Importaciones de modelos
 from user.models import CustomUser
@@ -28,3 +30,50 @@ class UserList(APIView):
         # serializer = ProcesoCreateUpdateSerializer(queryset,many=True,context={'request':request})
         return Response(self.custom_response("Success", queryset, status=status.HTTP_200_OK))
     
+class UserListDetail(APIView):
+    def custom_response(self, msg, response, status):
+        data ={
+            "messages": msg,
+            "pay_load": response,
+            "status": status,
+        }
+        res= json.dumps(data)
+        response = json.loads(res)
+        return response
+    
+    def get_object(self, id_users):
+        try:
+            return CustomUser.objects.get(pk = id_users)  
+        except CustomUser.DoesNotExist:   
+            return 0
+    
+    def get(self, request, id_users, format=None):
+        id_response = self.get_object(id_users)
+        if id_response != 0:
+            id_response = CustomUserDetailsSerializer(id_response)
+            return Response(self.custom_response("Success", id_response.data, status=status.HTTP_200_OK))
+        return Response(self.custom_response("Error", f"User with id: {id_users} not found", status=status.HTTP_400_BAD_REQUEST))
+    
+class UserDelete(APIView):
+    def custom_response(self, msg, response, status):
+        data ={
+            "messages": msg,
+            "pay_load": response,
+            "status": status,
+        }
+        res= json.dumps(data)
+        response = json.loads(res)
+        return response
+    
+    def get_object(self, id_users):
+        try:
+            return CustomUser.objects.get(pk = id_users)  
+        except CustomUser.DoesNotExist:   
+            return 0
+    
+    def delete(self, request, id_users, format=None):
+        id_response = self.get_object(id_users)
+        if id_response != 0:
+            id_response.delete()
+            return Response(self.custom_response("Success", f"User with id: {id_users} deleted", status=status.HTTP_200_OK))
+        return Response(self.custom_response("Error", f"User with id: {id_users} not found", status=status.HTTP_400_BAD_REQUEST))
