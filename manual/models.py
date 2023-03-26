1a49cec897fa385791574a607b60922af9b42863
Alan Gomez <soyalangg@gmail.com>
2023-03-25 23:41:04 -0600

    bug fix: manual and project api now can accept data

--- manual/models.py
+++ manual/models.py
@@ -5,14 +5,14 @@ from proyectos.models import ProyectosModel
 # Create your models here.
 
 class ManualModel(models.Model):
-    historia=models.CharField(max_length=300)
-    alcance = models.CharField(max_length=300)
-    vision = models.CharField(max_length=300)
-    mision=models.CharField(max_length=100)
+    historia=models.CharField(max_length=300, null=True)
+    alcance = models.CharField(max_length=300, null=True)
+    vision = models.CharField(max_length=300, null=True)
+    mision=models.CharField(max_length=100, null=True)
     organizacion_empresa = models.FileField(upload_to = 'resources',  blank = True, null=True)
     organigrama = models.FileField(upload_to = 'resources',  blank = True, null=True)
     vocabulario = models.FileField(upload_to = 'resources',  blank = True, null=True)
     funciones_puestos = models.FileField(upload_to = 'resources',  blank = True, null=True)
-    politicas = ArrayField(models.CharField(max_length=150))
+    politicas = ArrayField(models.CharField(max_length=150), null=True)
     proyecto = models.ForeignKey(ProyectosModel, on_delete=models.CASCADE, null=False, blank=False, related_name='proyecto')
     
--- manual/serializers.py
+++ manual/serializers.py
@@ -4,5 +4,4 @@ from manual.models import ManualModel
 class ManualSerializer(serializers.ModelSerializer):
     class Meta:
         model = ManualModel
-        # fields = 'historia', 'alcance', 'vision', 'mision', 'organizacion_empresa', 'organigrama', 'vocabulario', 'funciones_puestos', 'politicas', 'proyecto'
-        fields = '__all__'
+        fields = ('__all__')
