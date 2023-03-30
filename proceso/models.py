3842719737c3a087786ed110e73614745e0a0e65
191182CarlosMolina <191182@ids.upchiapas.edu.mx>
2023-03-30 01:24:42 -0600

    cambios para acceder a las imagenes

--- proceso/models.py
+++ proceso/models.py
@@ -12,7 +12,7 @@ class Proceso(models.Model):
     descripcion = ArrayField(models.TextField(blank=True), null=True)
     responsable = models.CharField(max_length=100, null=True)
     categoria = models.CharField(max_length=50, null=True)
-    participantes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='participantes_proceso')
+    participantes = ArrayField(models.CharField(max_length=100), null=True, blank=True)
     proceso_relacionado = models.CharField(max_length=100, null=True)
     frecuencia = models.CharField(max_length=100, null=True)
     status = models.BooleanField(null=True)
