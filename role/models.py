ee43242d2c6810bb7611334cb6ab4066dc434f60
Esbisans <193281@ids.upchiapas.edu.mx>
2023-04-01 23:11:37 -0600

    comment optimization

--- role/models.py
+++ role/models.py
@@ -4,4 +4,3 @@ from django.conf import settings
 
 class RoleModel(models.Model):
     name = models.CharField(null= True, max_length=200)
-    # id_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
